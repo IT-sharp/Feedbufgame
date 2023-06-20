6c8865c1cebef49d9658a75ae91ac21b0ee23a02
RickTh2407 <rick.thewissen@gmail.com>
2023-06-20 13:07:26 +0200

    20-06 part2

---
+++ FeedbufGame/BLL/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbufGame.BLL
+{
+    internal class Shop
+    {
+        private int Id { get; set; }
+        private string Image { get; set; }
+        private List<Shop> ShopList { get; set; }
+        public Shop(int id, string image)
+        {
+            Id = id;
+            Image = image;
+        }
+    }
+}
