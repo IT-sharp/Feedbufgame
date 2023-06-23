815e7fadbd5a0b71da4dd64a708ccf8c81465374
RickTh2407 <rick.thewissen@gmail.com>
2023-06-23 13:06:59 +0200

    23-06 (Part2)

--- FeedbufGame/Feedup.cs
+++ FeedbufGame/Feedup.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FeedbufGame
 {
-    public partial class Form1 : Form
+    public partial class Feedup : Form
     {
-        public Form1()
+        public Feedup()
         {
             InitializeComponent();
         }
