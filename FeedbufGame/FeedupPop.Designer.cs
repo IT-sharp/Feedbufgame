a80fd5216a75750d5996a3bbbb2437c9e6284c64
Luc-kn <knoppen.luc@gmail.com>
2023-06-30 13:18:14 +0200

    iets??

--- FeedbufGame/FeedupPop.Designer.cs
+++ FeedbufGame/FeedupPop.Designer.cs
@@ -70,6 +70,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(156, 22);
             this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "x";
             // 
             // label3
             // 
