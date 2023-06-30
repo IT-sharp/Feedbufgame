545238c0bcc8d7280cafced3e71730ca3804cf2a
RickTh2407 <rick.thewissen@gmail.com>
2023-06-30 15:31:29 +0200

    30-06 (part3)

--- FeedbufGame/BLL/Student.cs
+++ FeedbufGame/BLL/Student.cs
@@ -10,7 +10,7 @@ namespace FeedbufGame.BLL
     {
         private string Name { get; set; }
         private int PhoneNumber { get; set; }
-        private string EmailAddress { get; set; }
+        private string EmailAdress { get; set; }
         private int Points { get; set; }
         private List<Student> FeedupList { get; set; }
         private List<Student> Students { get; set; }
